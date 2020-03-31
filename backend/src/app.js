c434de6a12d203939241a893ef5f845fbb751101
Your Name <Matheus Couto>
2020-03-31 14:11:05 -0300

    Funcionalidades extras : Testes e Validações (incompleto)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const cors = require('cors');
+const { errors } = require ('celebrate');
 const routes = require('./routes')
 
 const  app = express();
@@ -7,6 +8,29 @@ const  app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
+
+
+module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  *  Rota / Recurso
  */
@@ -27,7 +51,3 @@ app.use(routes);
    * Route Parms: Parâmetros utilizados para identificar recursos
    * Request Body: Corpo da requisição, utilizado para 
    */
-
-
-
-app.listen(3333);
